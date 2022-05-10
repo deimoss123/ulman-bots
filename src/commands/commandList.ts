18914fd4a74291fac4e4e959fa1fa0677a654d53
deimoss123 <danielsalksnis@gmail.com>
2022-05-10 21:45:16 +0300

    parsaukta mape

--- src/commands/commandList.ts
+++ src/commands/commandList.ts
@@ -1,13 +1,13 @@
 import Command from '../interfaces/Command';
-import maks from './economy/maks/maks';
+import maks from './economyCommands/maks/maks';
 import _addLati from './_devCommands/_addLati/_addLati';
-import maksat from './economy/maksat/maksat';
+import maksat from './economyCommands/maksat/maksat';
 import _addItem from './_devCommands/_addItem/_addItem';
-import inventars from './economy/inventars/inventars';
-import iedot from './economy/iedot/iedot';
-import veikals from './economy/veikals/veikals';
-import pirkt from './economy/pirkt/pirkt';
-import pardot from './economy/pardot/pardot';
+import inventars from './economyCommands/inventars/inventars';
+import iedot from './economyCommands/iedot/iedot';
+import veikals from './economyCommands/veikals/veikals';
+import pirkt from './economyCommands/pirkt/pirkt';
+import pardot from './economyCommands/pardot/pardot';
 
 // komandu objektu saraksts
 export const commandList: Command[] = [
