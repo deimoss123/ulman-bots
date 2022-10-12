64cc263b02e7f9b0d8ae244a04928dcc3571c379
deimoss123 <danielsalksnis@gmail.com>
2022-10-12 03:31:24 +0300

    nomainīts bota statuss

--- src/utils/setBotPresence.ts
+++ src/utils/setBotPresence.ts
@@ -1,6 +1,6 @@
 import { ActivityType, Client, PresenceUpdateStatus } from 'discord.js';
 
 export default function setBotPresence(client: Client) {
-  client.user?.setActivity(`/ulmaņbots | ${client.guilds.cache.size} serveros`, { type: ActivityType.Playing });
+  client.user?.setActivity(`/palidziba | ${client.guilds.cache.size} serveros`, { type: ActivityType.Playing });
   client.user?.setPresence({ status: PresenceUpdateStatus.Online });
 }
