1b779394e9651f688129b5ce77437e33840c1b38
deimoss123 <danielsalksnis@gmail.com>
2022-10-12 16:34:10 +0300

    salaboti dizloto laimesti

--- src/casino/laimesti/laimestiDizloto.ts
+++ src/casino/laimesti/laimestiDizloto.ts
@@ -3,25 +3,25 @@ import { ChanceRecord } from '../../items/helpers/chance';
 const laimestiDizloto: ChanceRecord = {
   win_massive: {
     chance: 0.01,
-    reward: 4000,
+    reward: 5000,
     name: '**!!! MILZĪGO !!!**',
     color: 0xeb44ff,
   },
   win_big: {
     chance: 0.1,
-    reward: 800,
+    reward: 1200,
     name: '**! LIELO !**',
     color: 0x45e8e8,
   },
   win_medium: {
     chance: 0.2,
-    reward: 400,
+    reward: 700,
     name: '**vidējo**',
     color: 0x69ee54,
   },
   win_small: {
     chance: 0.45,
-    reward: 170,
+    reward: 350,
     name: 'mazo',
     color: 0xf6d179,
   },
