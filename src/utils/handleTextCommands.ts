40f4a74011ca6bd91fd0eb270b11ed6e929d929a
deimoss123 <danielsalksnis@gmail.com>
2022-10-14 14:07:51 +0300

    gramatikas labojums

--- src/utils/handleTextCommands.ts
+++ src/utils/handleTextCommands.ts
@@ -79,9 +79,9 @@ export default function handleTextCommands(msg: Message) {
             .setDescription(
               'Bet tie nav vienīgie jaunumi, ir arī izlaists lielākais atjauninājums līdz šim, ' +
                 'ko vari apskatīt ar komandu `/palidziba jaunumi`, veiktas daudz izmaiņas, daudzi uzlabojumi, pievienotas jaunas mantas un komandas.\n\n' +
-                'Ar šo atjauninājumu nāk arī __**ekonomijas resets**__, tiem kam šis ir pārsteigums, ' +
+                'Ar šo atjauninājumu nāk arī __**ekonomikas resets**__, tiem kam šis ir pārsteigums, ' +
                 'varat droši pievienoties OkDraudziņDauni serverim, kur var saņemt UlmaņBota paziņojumus laicīgi - <https://discord.gg/F4s5AwYTMy> \n' +
-                'Šajā serverī ir visaktīvākie un draudzīgākie UlmaņBota lietotāji, kā arī vislielākā ekonomija.\n\n' +
+                'Šajā serverī ir visaktīvākie un draudzīgākie UlmaņBota lietotāji, kā arī vislielākā ekonomika.\n\n' +
                 'Ja slīpsvītru (/) komandas nav parādījušās jūsu serverī, pievienojiet botu no jauna ar šo linku: \n' +
                 '> <https://discord.com/api/oauth2/authorize?client_id=892747599143125022&permissions=262144&scope=bot%20applications.commands>\n' +
                 'vai arī atverot bota profilu un uzspiežot uz pogas "Add to Server".\n\n' +
