188b36538c1bf8c5e54b51618ca5154a23c24959
deimoss123 <danielsalksnis@gmail.com>
2022-10-14 03:22:32 +0300

    pievienota veco teksta komandu pārbaude

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import autocompleteHandler from './commands/autocompleteHandler';
 import chalk from 'chalk';
 import setBotPresence from './utils/setBotPresence';
 import createTirgus from './items/tirgus/createTirgus';
+import handleTextCommands from './utils/handleTextCommands';
 
 process.env.TZ = 'Europe/Riga';
 dotenv.config();
@@ -16,19 +17,25 @@ dotenv.config();
 // pārbauda vai .env failā ir ievadīti mainīgie
 if (!validateEnv()) process.exit(1);
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages],
+});
 
-client.once('ready', () => {
+client.once('ready', async () => {
   createDiscounts();
   createTirgus();
   setBotPresence(client);
   setupCronJobs(client);
-  mongo().then(() => console.log('Connected to MongoDB'));
-});
+  await mongo().then(() => console.log('Connected to MongoDB'));
 
-client.on('interactionCreate', i => {
-  if (i.isChatInputCommand()) commandHandler(i);
-  else if (i.isAutocomplete()) autocompleteHandler(i);
+  client.on('messageCreate', msg => {
+    handleTextCommands(msg);
+  });
+
+  client.on('interactionCreate', i => {
+    if (i.isChatInputCommand()) commandHandler(i);
+    else if (i.isAutocomplete()) autocompleteHandler(i);
+  });
 });
 
 client.login(process.env.BOT_TOKEN).then(() => {
