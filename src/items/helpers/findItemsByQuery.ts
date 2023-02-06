a0f87be99e2cc0af24b12035f7b9e34a45e8c213
deimoss123 <danielsalksnis@gmail.com>
2023-02-06 20:12:14 +0200

    labāka mantu meklēšana

--- src/items/helpers/findItemsByQuery.ts
+++ src/items/helpers/findItemsByQuery.ts
@@ -1,41 +1,17 @@
 import Item from '../../interfaces/Item';
-import stringSimilarity from 'string-similarity';
 import normalizeLatText from '../../embeds/helpers/normalizeLatText';
+import { search } from 'fast-fuzzy';
 
-type ItemWithRating = [string, Item, number];
+export default function findItemsByQuery(query: string, itemsToQuery: [string, Item][]): [string, Item][] {
+  const fuzzyRes = search(query, itemsToQuery, {
+    keySelector: obj => normalizeLatText(obj[1].nameNomVsk),
+    ignoreCase: true,
+    returnMatchData: true,
+  });
 
-function removeRating(item: ItemWithRating): [string, Item] {
-  return [item[0], item[1]];
-}
-
-function filterGreaterThan(num: number) {
-  return (item: ItemWithRating) => item[2] >= num - 0.2;
-}
-
-export default function findItemsByQuery(
-  query: string,
-  itemsToQuery: [string, Item][]
-): [string, Item][] {
-  const itemsToQueryNames = itemsToQuery.map(([, item]) => normalizeLatText(item.nameNomVsk));
-  const queryResult = stringSimilarity.findBestMatch(query, itemsToQueryNames);
-
-  if (queryResult.bestMatch.rating === 0) {
+  if (!query.length || !fuzzyRes.length) {
     return itemsToQuery.slice(0, 25);
   }
 
-  // pievieno reitingu katrai mantai sarakstā
-  const itemsWithRatings: ItemWithRating[] = queryResult.ratings.map(({ rating }, index) => [
-    ...itemsToQuery[index],
-    rating,
-  ]);
-
-  // sakārto pēc reitinga
-  const sortedItemsWithRatings = itemsWithRatings.sort((a, b) => b[2] - a[2]);
-
-  // console.log(sortedItemsWithRatings.map(a => [a[0], a[2]]));
-
-  return sortedItemsWithRatings
-    .filter(filterGreaterThan(queryResult.bestMatch.rating))
-    .map(removeRating)
-    .slice(0, 25);
+  return fuzzyRes.map(obj => obj.item).slice(0, 25);
 }
