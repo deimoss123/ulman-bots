0a5a63c2e7bef0bf07998c477a89118d785aecfe
deimoss123 <danielsalksnis@gmail.com>
2023-05-14 14:25:50 +0300

    /top paginācija

--- src/commands/economyCommands/top/top.ts
+++ src/commands/economyCommands/top/top.ts
@@ -5,11 +5,17 @@ import topData from './topData';
 import errorEmbed from '../../../embeds/errorEmbed';
 import topEmbed from './topEmbed';
 import findUser from '../../../economy/findUser';
-import { sortDataProfile, sortDataStats } from './sortData';
+import { SortDataProfileEntry, sortDataProfile, sortDataStats } from './sortData';
 import getStatsMany from '../../../economy/stats/getStatsMany';
 import intReply from '../../../utils/intReply';
+import StatsProfile from '../../../interfaces/StatsProfile';
+import UserProfile from '../../../interfaces/UserProfile';
+import buttonHandler from '../../../embeds/buttonHandler';
+import btnPaginationRow from '../../../items/helpers/btnPaginationRow';
+import { ComponentType } from 'discord.js';
 
-export const TOP_LIMIT = 10;
+export const TOP_USERS_PER_PAGE = 10;
+export const TOP_MAX_PAGES = 10;
 
 const top: Command = {
   description: 'Apskatīt severa lietotāja topu',
@@ -18,6 +24,7 @@ const top: Command = {
   async run(i) {
     const userId = i.user.id;
     const guildId = i.guildId!;
+    // const guildId = '797584379685240882'; // okdd id
 
     const category = i.options.getString('kategorija')!;
 
@@ -30,59 +37,109 @@ const top: Command = {
     const defer = i.deferReply().catch(_ => _);
 
     const user = await findUser(userId, i.guildId!);
-
     if (!user) {
       await defer;
       return i.editReply(errorEmbed).catch(_ => _);
     }
 
+    // @ts-ignore
+    // prettier-ignore
+    const sort: {
+      allUsers: StatsProfile[] | undefined;
+      sortDataObj: SortDataProfileEntry<StatsProfile>;
+    } | {
+      allUsers: UserProfile[] | undefined;
+      sortDataObj: SortDataProfileEntry<UserProfile>;
+    } = {};
+
     if (isStatsCategory) {
-      const sortDataObj = sortDataStats[category];
-      if (!sortDataObj) {
+      sort.sortDataObj = sortDataStats[category];
+      if (!sort.sortDataObj) {
         await defer;
         return i.editReply(errorEmbed).catch(_ => _);
       }
 
-      const { projection, sortFunc } = sortDataObj;
-      const allUsers = await getStatsMany(i.client.user.id, guildId, projection);
-      if (!allUsers) {
+      const { projection } = sort.sortDataObj;
+      sort.allUsers = await getStatsMany(i.client.user.id, guildId, projection);
+    } else {
+      sort.sortDataObj = sortDataProfile[category];
+      if (!sort.sortDataObj) {
         await defer;
         return i.editReply(errorEmbed).catch(_ => _);
       }
 
-      const sortedUsers = allUsers.sort(sortFunc);
-
-      const usersToFetch = sortedUsers.slice(0, TOP_LIMIT).map(user => user.userId);
-      if (!usersToFetch.includes(userId)) usersToFetch.push(userId);
-
-      await Promise.all([i.guild!.members.fetch({ user: usersToFetch }), defer]);
-
-      return i.editReply(topEmbed(i, categoryTitle, sortedUsers, sortDataObj)).catch(_ => _);
+      const { projection } = sort.sortDataObj;
+      sort.allUsers = await getAllUsers(i.client.user!.id, guildId, projection);
     }
 
-    // koda atkārtošana jaaaaaaaaa, es padevos saprast tipuskriptu lai šo īsāk uzrakstīt
-    const sortDataObj = sortDataProfile[category];
-    if (!sortDataObj) {
+    if (!sort.allUsers) {
       await defer;
       return i.editReply(errorEmbed).catch(_ => _);
     }
 
-    const { projection, sortFunc } = sortDataObj;
+    const { sortDataObj } = sort; // @ts-ignore
+    const sortedUsers = sort.allUsers.sort(sortDataObj.sortFunc);
 
-    const allUsers = await getAllUsers(i.client.user!.id, guildId, projection);
-    if (!allUsers) {
-      await defer;
-      return i.editReply(errorEmbed).catch(_ => _);
-    }
-
-    const sortedUsers = allUsers.sort(sortFunc);
-
-    const usersToFetch = sortedUsers.slice(0, TOP_LIMIT).map(user => user.userId);
+    const usersToFetch = sortedUsers.slice(0, TOP_USERS_PER_PAGE * TOP_MAX_PAGES).map(user => user.userId);
     if (!usersToFetch.includes(userId)) usersToFetch.push(userId);
 
     await Promise.all([i.guild!.members.fetch({ user: usersToFetch }), defer]);
 
-    return i.editReply(topEmbed(i, categoryTitle, sortedUsers, sortDataObj)).catch(_ => _);
+    if (sortedUsers.length <= TOP_USERS_PER_PAGE) {
+      // @ts-ignore
+      return i.editReply(topEmbed(i, categoryTitle, 0, sortedUsers, sortDataObj)).catch(_ => _);
+    }
+
+    let currentPage = 0;
+    const totalPages =
+      Math.ceil(sortedUsers.length / TOP_USERS_PER_PAGE) >= TOP_MAX_PAGES
+        ? TOP_MAX_PAGES
+        : Math.ceil(sortedUsers.length / TOP_USERS_PER_PAGE);
+
+    const msg = await i
+      .editReply({
+        // @ts-ignore
+        embeds: topEmbed(i, categoryTitle, currentPage, sortedUsers, sortDataObj).embeds,
+        components: [btnPaginationRow('top', currentPage, totalPages)],
+      })
+      .catch();
+    if (!msg) return;
+
+    await buttonHandler(
+      i,
+      'top',
+      msg,
+      async int => {
+        const { customId, componentType } = int;
+        if (componentType !== ComponentType.Button) return;
+
+        switch (customId) {
+          case 'top_prev_page': {
+            currentPage--;
+            if (currentPage < 0) currentPage = 0;
+            return {
+              edit: {
+                // @ts-ignore
+                embeds: topEmbed(i, categoryTitle, currentPage, sortedUsers, sortDataObj).embeds,
+                components: [btnPaginationRow('top', currentPage, totalPages)],
+              },
+            };
+          }
+          case 'top_next_page': {
+            currentPage++;
+            if (currentPage >= totalPages) currentPage = totalPages - 1;
+            return {
+              edit: {
+                // @ts-ignore
+                embeds: topEmbed(i, categoryTitle, currentPage, sortedUsers, sortDataObj).embeds,
+                components: [btnPaginationRow('top', currentPage, totalPages)],
+              },
+            };
+          }
+        }
+      },
+      60000
+    );
   },
 };
 
--- src/commands/economyCommands/top/topEmbed.ts
+++ src/commands/economyCommands/top/topEmbed.ts
@@ -6,21 +6,27 @@ import StatsProfile from '../../../interfaces/StatsProfile';
 import UserProfile from '../../../interfaces/UserProfile';
 import { displayPlace } from '../statistika/statistika';
 import { SortDataProfileEntry } from './sortData';
-import { TOP_LIMIT } from './top';
+import { TOP_USERS_PER_PAGE } from './top';
 
 export default function topEmbed<T extends UserProfile | StatsProfile>(
   i: ChatInputCommandInteraction,
   title: string,
+  currentPage: number,
   sortedUsers: T[],
   { displayValue, totalReduceFunc, partOfTotal, topDescription }: SortDataProfileEntry<T>
 ) {
   const total = totalReduceFunc && sortedUsers.reduce(totalReduceFunc, 0);
 
-  const fields = sortedUsers.slice(0, TOP_LIMIT).map((user, index) => {
+  const offset = TOP_USERS_PER_PAGE * currentPage;
+  const slicedUsers = sortedUsers.slice(offset, offset + TOP_USERS_PER_PAGE);
+
+  const fields = slicedUsers.map((user, index) => {
     return {
       name:
         (user.userId === i.user.id ? `${iconEmojis.blueArrowRight} ` : '') +
-        `${displayPlace(index)} ${i.guild!.members.cache.get(user.userId)?.user.tag || 'Nezināms lietotājs'} ` +
+        `${displayPlace(index + offset)} ${
+          i.guild!.members.cache.get(user.userId)?.user.tag || 'Nezināms lietotājs'
+        } ` +
         (total ? `\`${(partOfTotal!(total, user) * 100).toFixed(2)}%\`` : ''),
       value: displayValue(user),
       inline: false,
@@ -29,10 +35,10 @@ export default function topEmbed<T extends UserProfile | StatsProfile>(
 
   const indexOf = sortedUsers.findIndex(u => u.userId === i.user.id);
 
-  if (indexOf > TOP_LIMIT - 1) {
+  if (!slicedUsers.find(u => u.userId === i.user.id)) {
     const foundUser = sortedUsers.find(user => user.userId === i.user.id)!;
 
-    fields[TOP_LIMIT - 1].value += `\n__${'\u2800'.repeat(20)}__`;
+    fields[fields.length - 1].value += `\n__${'\u2800'.repeat(20)}__`;
     fields.push({
       name:
         `${iconEmojis.blueArrowRight} ${displayPlace(indexOf)} ${i.user.tag} ` +
