3d9aab0b0bc9584add550f608bc3b2b51c3e70b9
daniels <danielsalksnis@gmail.com>
2024-02-24 02:45:35 +0200

    iekomentēt lai TS nebļauj

--- src/items/usableItems/zivju_kaste.ts
+++ src/items/usableItems/zivju_kaste.ts
@@ -10,14 +10,14 @@ interface IZivs {
   };
 }
 
-const zivjuKastesZivjuSaraksts: Record<ItemKey, IZivs> = {
-  makrele_a: {
-    attributes: {
-      weight: [5, 10],
-    },
-  },
-  forele_a: {},
-};
+// const zivjuKastesZivjuSaraksts: Record<ItemKey, IZivs> = {
+//   makrele_a: {
+//     attributes: {
+//       weight: [5, 10],
+//     },
+//   },
+//   forele_a: {},
+// };
 
 const zivju_kaste: UsableItemFunc = async (userId, guildId) => {
   const user = await findUser(userId, guildId);
