7871198654a2d3fbf7b0dff383b6fcc937995a25
deimoss123 <danielsalksnis@gmail.com>
2024-03-06 21:55:43 +0200

    Paplašināt chance funkciju

--- src/items/helpers/chance.ts
+++ src/items/helpers/chance.ts
@@ -28,7 +28,78 @@ export type ChanceRecord = Record<string, ChanceObj>;
 
 // nejauši izvēlās kādu vērtību no chance objekta
 // ! CHANCE SUMMA OBJEKTĀ NEDRĪKST PĀRSNIEGT 1 !
-export default function chance(obj: ChanceRecord): ChanceReturn {
+function chance(data: ChanceObj[]): ChanceObj;
+function chance(data: ChanceRecord): ChanceReturn;
+function chance(data: ChanceObj[] | ChanceRecord): ChanceObj | ChanceReturn {
+  const randNum = Math.random();
+  // svaru summa
+  let sumChance = 0;
+
+  // masīvs kas satur objektus ar '*' svaru
+  const starredArr = [];
+
+  // iziet cauri visiem svariem
+  if (Array.isArray(data)) {
+    let result: ChanceObj | null = null;
+
+    for (const obj of data) {
+      // ja svars ir '*' tas tiek pievienots starredArr
+      if (obj.chance === '*') {
+        starredArr.push(obj);
+        continue;
+      }
+
+      // svaru summai tiek pievienots esošā objekta svars
+      sumChance += obj.chance as number;
+
+      // ja randNum ir mazāks vai vienāds par svaru summu tad tas tiek izvēlēts kā rezultāts
+      if (randNum <= sumChance) {
+        result = obj;
+        break;
+      }
+    }
+    //
+    // ja netiek izvēlēts objets no objektiem kuru svars ir skaitlis,
+    // tad nejauši tiek izvēlēts viens no objektiem ar '*' svaru
+    if (!result) {
+      result = starredArr[Math.floor(Math.random() * starredArr.length)];
+    }
+
+    return result;
+  } else {
+    let resultKey = '';
+
+    for (const [key, obj] of Object.entries(data)) {
+      // ja svars ir '*' tas tiek pievienots starredArr
+      if (obj.chance === '*') {
+        starredArr.push(key);
+        continue;
+      }
+
+      // svaru summai tiek pievienots esošā objekta svars
+      sumChance += obj.chance as number;
+
+      // ja randNum ir mazāks vai vienāds par svaru summu tad tas tiek izvēlēts kā rezultāts
+      if (randNum <= sumChance) {
+        resultKey = key;
+        break;
+      }
+
+      // ja netiek izvēlēts objets no objektiem kuru svars ir skaitlis,
+      // tad nejauši tiek izvēlēts viens no objektiem ar '*' svaru
+      if (!resultKey) {
+        resultKey = starredArr[Math.floor(Math.random() * starredArr.length)];
+      }
+    }
+
+    return {
+      key: resultKey,
+      obj: data[resultKey],
+    };
+  }
+}
+
+export default function chance2(obj: ChanceRecord): ChanceReturn {
   const randNum = Math.random();
 
   let result;
