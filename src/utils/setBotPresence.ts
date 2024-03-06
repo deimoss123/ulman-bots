ff2a3ce92fd539203930557e4e45dbc621a58a91
deimoss123 <danielsalksnis@gmail.com>
2024-03-06 13:44:51 +0200

    Statuss kas līdzinās maniem pārējiem botiem

--- src/utils/setBotPresence.ts
+++ src/utils/setBotPresence.ts
@@ -1,6 +1,16 @@
 import { ActivityType, Client, PresenceUpdateStatus } from 'discord.js';
 
-export default function setBotPresence(client: Client) {
-  client.user?.setActivity(`/palidziba | ${client.guilds.cache.size} serveros`, { type: ActivityType.Playing });
-  client.user?.setPresence({ status: PresenceUpdateStatus.Online });
+export default function setBotPresence(client: Client<true>) {
+  const guildCount = client.guilds.cache.size;
+
+  client.user.setPresence({
+    status: PresenceUpdateStatus.Online,
+    activities: [
+      {
+        state: `/palidziba | ${guildCount} serveros`,
+        type: ActivityType.Custom,
+        name: '-',
+      },
+    ],
+  });
 }
