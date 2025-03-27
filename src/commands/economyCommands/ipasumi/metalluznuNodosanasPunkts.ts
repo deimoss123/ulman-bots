4de84775ee1209af48567d7db814da41d051a54a
deimoss123 <danielsalksnis@gmail.com>
2025-03-27 13:58:10 +0200

    grafiku ģenerēšana granulu katlam

--- src/commands/economyCommands/ipasumi/metalluznuNodosanasPunkts.ts
+++ src/commands/economyCommands/ipasumi/metalluznuNodosanasPunkts.ts
@@ -5,6 +5,7 @@ import {
   ButtonBuilder,
   ButtonInteraction,
   ButtonStyle,
+  resolveColor,
   StringSelectMenuInteraction,
 } from 'discord.js';
 import { IpasumiState } from './ipasumi';
@@ -13,42 +14,61 @@ import UserProfile from '../../../interfaces/UserProfile';
 import setUser from '../../../economy/setUser';
 import findUser from '../../../economy/findUser';
 import emoji from '../../../utils/emoji';
-import { writeFile } from 'fs/promises';
+import { writeFile, readFile } from 'fs/promises';
 import { Dialogs } from '../../../utils/Dialogs';
 import mongoTransaction from '../../../utils/mongoTransaction';
 import addLati from '../../../economy/addLati';
 import intReply from '../../../utils/intReply';
 import smallEmbed from '../../../embeds/smallEmbed';
 import latiString from '../../../embeds/helpers/latiString';
+import { Canvas, createCanvas, GlobalFonts, SKRSContext2D } from '@napi-rs/canvas';
+import { join } from 'path';
+
+const fontPath = join(__dirname, '..', '..', '..', '..', 'assets', 'fonts');
+
+// fontos beigās 2, lai pārliecinātos, ka izmanto ielādētos, nevis sistēmas
+GlobalFonts.registerFromPath(join(fontPath, 'Inter-VariableFont_opsz,wght.ttf'), 'Inter2');
+
+// gatavo json datu tips
+interface MetalluznuData {
+  OPTIMAL_TEMPERATURE: number;
+  CURVE_STANDARD_DEVIATION: number;
+  MAX_EARNING_RATE: number;
+  data: [number, number][];
+}
+
+// iegūst iepriekš ģenerētos līknes datus zīmēšanai
+const jsonData = JSON.parse(await readFile('metalluznuNodosanasPunktsData.json', 'utf-8')) as MetalluznuData;
 
 const enum Config {
-  INITIAL_TEMPERATURE = -20, // Starting temperature in Celsius
-  GRANULE_HEAT_INCREASE = 5, // Temperature increase per granule
-  TEMPERATURE_DECAY_PER_HOUR = 1, // Temperature loss per hour
+  INITIAL_TEMPERATURE = -20, // sākuma temperatūra
+  GRANULE_HEAT_INCREASE = 15, // par cik grādiem 1 granula palielina temperatūru
+  TEMPERATURE_DECAY_PER_HOUR = 1, // temperatūras zudums katru stundu
 
   MIN_TEMPERATURE = -20,
   MAX_TEMPERATURE = 50,
 
-  BASE_MAX_LATI = 1000,
+  BASE_MAX_LATI = 1000, // bāzes maksimālais latu skaits
 
-  // Earnings curve parameters
-  OPTIMAL_TEMPERATURE = 22, // Temperature with maximum earnings
-  CURVE_STANDARD_DEVIATION = 15, // Controls the width of the normal distribution
-  MAX_EARNING_RATE = 10, // Maximum earnings per hour at optimal temperature
+  // normālsadalījuma iestatījumi
+  OPTIMAL_TEMPERATURE = 22, // līknes virsotne (lielākā peļņa)
+  CURVE_STANDARD_DEVIATION = 15,
+  MAX_EARNING_RATE = 10, // maksimālā peļņa optimālajā temperatūrā
 
-  // MS_IN_ONE_HOUR = 3600000, // millis in one hour
-  MS_IN_ONE_HOUR = 1000,
+  // MS_IN_ONE_HOUR = 3600000, // milisekundes stundā
+  MS_IN_ONE_HOUR = 1000, // testēšanai
 }
 
+// ģenerē statiskos datus json failā, lai līknes dati nebūtu jāģenerē katru reizi zīmējot grafiku
 async function generateChartData() {
   const arr: [number, number][] = [];
 
   // reiz 10 citādāk peldošais punkts izjokos ar 0.1 + 0.2 = 0.30000000000000004
-  for (let i = -200; i <= 500; i++) {
-    arr.push([i / 10, calculateEarningRatePerHour(i / 10)]);
+  for (let i = -20; i <= 50; i++) {
+    arr.push([i / 1, calculateEarningRatePerHour(i / 1)]);
   }
 
-  const obj = {
+  const obj: MetalluznuData = {
     OPTIMAL_TEMPERATURE: Config.OPTIMAL_TEMPERATURE,
     CURVE_STANDARD_DEVIATION: Config.CURVE_STANDARD_DEVIATION,
     MAX_EARNING_RATE: Config.MAX_EARNING_RATE,
@@ -58,9 +78,302 @@ async function generateChartData() {
   await writeFile('metalluznuNodosanasPunktsData.json', JSON.stringify(obj, null, 2), 'utf-8');
 }
 
+// tips charta novietojumam iekš canvasa 
+interface ChartArea {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+// globālie mainīgie statiskajam canvasam
+let staticCanvas: Canvas | null = null;
+let staticCtx: SKRSContext2D | null = null;
+let isStaticCanvasInitialized = false;
+let chartArea: ChartArea | null = null;
+
+const chartConfig = {
+  // bildes beigu izmērs
+  // (charta izmērs tiek aprēķināts atkarībā no pārējiem mainīgajiem)
+  width: 600,
+  height: 300,
+
+  fontFamily: 'Inter2',
+  labelFontSize: 22,
+  tickFontSize: 16,
+
+  tickLength: 8,
+  tickLabelGap: 4, // distance starp ticku un ticka tekstu
+  axisLabelGap: 8, // distance starp ticka tekstu un axis labelu
+
+  /*
+   * ──┬────┬────┬────┬────┬───┬──
+   *   │    │    │    │    │   │   <-- tickLength
+   *                               <-- tickLabelGap
+   *   0    5    10   15   20  25  <-- tickFontSize
+   *                               <-- axisLabelGap
+   *        Temperatūra, °C        <-- labelFontSize
+   *                               <-- padding.bottom
+   * -------(attēla apakša)-------
+   */
+
+  // ārējais paddings
+  padding: {
+    top: 28,
+    right: 28,
+    bottom: 16,
+    left: 8,
+  },
+  colors: {
+    background: '#18181b',
+    axis: '#fafafa',
+    axisTicks: '#fafafa',
+    tickLabels: '#fafafa',
+    axisLabels: '#d4d4d4',
+    dot: '#fafafa',
+    indicatorLine: '#52525b',
+  },
+};
+
+// temperatūras iedaļas tekstam un līknes krāsiņai
+// katrs iedaļa attiecas uz temperatūru kas ir zemāka par pirmo elementu (temperatūru)
+const tempSegments: [number, { text: string; color: string }][] = [
+  [0, { text: 'Auksts', color: '#1d4ed8' }], // zem 0 grādiem
+  [10, { text: 'Vēss', color: '#0891b2' }], // zem 10 grādiem
+  [16, { text: 'Remdens', color: '#2dd4bf' }], // utt.
+  [26, { text: 'Optimāls siltums', color: '#4ade80' }],
+  [35, { text: 'Ļoti karsts', color: '#facc15' }],
+  [42, { text: 'Bīstami karsts', color: '#f97316' }],
+  [Infinity, { text: 'Pārkaršana', color: '#b91c1c' }],
+];
+
+// aprēķina kur tiks novietots pats charts
+// šis ir vajadzīgs, jo configā var atšķirties paddings/fonta izmērs un tas ietekmē paša charta novietojumu
+function calculateChartArea(ctx: SKRSContext2D): ChartArea {
+  const { width, height, padding, labelFontSize, tickFontSize, tickLength, tickLabelGap, axisLabelGap } = chartConfig;
+  const { MAX_EARNING_RATE } = jsonData;
+
+  ctx.font = `${tickFontSize}px ${chartConfig.fontFamily}`;
+
+  // Aprēķina maksimālo y ticka izmēru
+  // tam nevar izmantot fonta izmēru, jo tas ir horizontālais garums
+  // ticku izmēri var atšķirties
+  // piemēram, 0 ir īsāks par 10, tāpēc jāatrod garākais
+  let maxYTickWidth = 0;
+  const yTickCount = 5;
+  for (let i = 0; i <= yTickCount; i++) {
+    const rate = (i / yTickCount) * MAX_EARNING_RATE;
+    const textWidth = ctx.measureText(rate.toString()).width;
+    maxYTickWidth = Math.max(maxYTickWidth, textWidth);
+  }
+
+  // nobīde kreisajā pusē
+  const leftSpace = tickLength + tickLabelGap + maxYTickWidth + axisLabelGap + labelFontSize;
+
+  // nobīde lejā
+  const bottomSpace = tickLength + tickLabelGap + tickFontSize + axisLabelGap + labelFontSize;
+
+  // "paddings" iekļaujot labelus, tickus un paddingu
+  const effectivePadding = {
+    top: padding.top,
+    right: padding.right,
+    bottom: padding.bottom + bottomSpace,
+    left: padding.left + leftSpace,
+  };
+
+  // charta novietojums canvasā
+  return {
+    x: effectivePadding.left,
+    y: effectivePadding.top,
+    width: width - effectivePadding.left - effectivePadding.right,
+    height: height - effectivePadding.top - effectivePadding.bottom,
+  };
+}
+
+// palīgfunkcijas lai pareizo novietojumu canvasā iekš charta
+const xScale = (temp: number) => chartArea!.x + ((temp + 20) / 70) * chartArea!.width;
+const yScale = (rate: number) => chartArea!.y + chartArea!.height - (rate / Config.MAX_EARNING_RATE) * chartArea!.height;
+
+// uzzīmē statisko daļu
+// izņemot pašu līkni un axis līnijas, jo tās jāzīmē pāri indikatora līnijām
+async function initializeStaticChart(): Promise<void> {
+  const { width, height, tickLength, tickLabelGap, labelFontSize, padding, colors } = chartConfig;
+
+  staticCanvas = createCanvas(width, height);
+  staticCtx = staticCanvas.getContext('2d');
+
+  staticCtx.fillStyle = colors.background;
+  staticCtx.fillRect(0, 0, width, height);
+
+  chartArea = calculateChartArea(staticCtx);
+
+  // axis labeļu stili
+  staticCtx.fillStyle = colors.axisLabels;
+  staticCtx.font = `${labelFontSize}px ${chartConfig.fontFamily}`;
+  staticCtx.textAlign = 'center';
+
+  // x-axis labelis
+  staticCtx.fillText('Temperatūra, °C', chartArea.x + chartArea.width / 2, height - padding.bottom);
+
+  // y-axis labelis
+  // stulbais canvas neļauj pagriezt tekstu
+  // tāpēc jāgriež viss canvass, jāuzraksta teksts un jāgriež atpakaļ
+  staticCtx.save();
+  staticCtx.translate(padding.left + labelFontSize, chartArea.y + chartArea.height / 2);
+  staticCtx.rotate(-Math.PI / 2);
+  staticCtx.fillText('Peļņa, lati/h', 0, 0);
+  staticCtx.restore();
+
+  staticCtx.font = `${chartConfig.tickFontSize}px ${chartConfig.fontFamily}`;
+
+  // ticku un ticku leibeļu stili
+  staticCtx.strokeStyle = colors.axisTicks;
+  staticCtx.fillStyle = colors.tickLabels;
+  staticCtx.lineWidth = 2;
+
+  // zīmē x-axis tickus un ticku leibeļus
+  for (let temp = -20; temp <= 50; temp += 10) {
+    const x = xScale(temp);
+
+    // ticks
+    staticCtx.beginPath();
+    staticCtx.moveTo(x, chartArea.y + chartArea.height);
+    staticCtx.lineTo(x, chartArea.y + chartArea.height + tickLength);
+    staticCtx.stroke();
+
+    // ticka leibelis
+    staticCtx.textAlign = 'center';
+    staticCtx.fillText(
+      temp.toString(),
+      x,
+      chartArea.y + chartArea.height + tickLength + tickLabelGap + chartConfig.tickFontSize,
+    );
+  }
+
+  // zīmē y-axis tickus un ticku leibeļus
+  const yTickCount = 5;
+  for (let i = 0; i <= yTickCount; i++) {
+    const rate = (i / yTickCount) * Config.MAX_EARNING_RATE;
+    const y = yScale(rate);
+
+    // ticks
+    staticCtx.beginPath();
+    staticCtx.moveTo(chartArea.x, y);
+    staticCtx.lineTo(chartArea.x - tickLength, y);
+    staticCtx.stroke();
+
+    // ticka leibelis
+    staticCtx.textAlign = 'right';
+    staticCtx.fillText(
+      rate.toString(),
+      chartArea.x - tickLength - tickLabelGap,
+      y + chartConfig.tickFontSize / 3, // Vertical centering adjustment
+    );
+  }
+
+  isStaticCanvasInitialized = true;
+}
+
+// uzzīmē statiskajai daļai pa virsu dinamiskos elementus
+// atgriež fināla chartu
+async function generateChart(temperature: number): Promise<Buffer> {
+  // TODO: salīdzināt json faila datus, ja tie nesakrīt ar configa mainīgajiem - pārģenerēt
+  if (!isStaticCanvasInitialized) {
+    await initializeStaticChart();
+  }
+
+  const { width, height, colors } = chartConfig;
+  const { data } = jsonData;
+
+  const canvas = createCanvas(width, height);
+  const ctx = canvas.getContext('2d');
+
+  ctx.drawImage(staticCanvas!, 0, 0);
+
+  const chartArea = calculateChartArea(ctx);
+
+  const currentRate = calculateEarningRatePerHour(temperature);
+  const currentY = yScale(currentRate);
+  const currentX = xScale(temperature);
+
+  // indikatora līnijas stili
+  ctx.strokeStyle = colors.indicatorLine;
+  ctx.lineWidth = 2;
+  ctx.setLineDash([5, 3]);
+
+  // zīmē vertikālo indikatora līniju
+  ctx.beginPath();
+  ctx.moveTo(currentX, currentY);
+  ctx.lineTo(currentX, chartArea.y + chartArea.height);
+  ctx.stroke();
+
+  // zīmē horizontālo indikatora līniju
+  ctx.beginPath();
+  ctx.moveTo(currentX, currentY);
+  ctx.lineTo(chartArea.x, currentY);
+  ctx.stroke();
+
+  ctx.setLineDash([]);
+
+  // axis līniju stili
+  ctx.strokeStyle = colors.axis;
+  ctx.lineWidth = 1;
+
+  // zīmē x-axis līniju
+  ctx.beginPath();
+  ctx.moveTo(chartArea.x, chartArea.y + chartArea.height);
+  ctx.lineTo(chartArea.x + chartArea.width, chartArea.y + chartArea.height);
+  ctx.stroke();
+
+  // zīmē y-axis līniju
+  ctx.beginPath();
+  ctx.moveTo(chartArea.x, chartArea.y);
+  ctx.lineTo(chartArea.x, chartArea.y + chartArea.height);
+  ctx.stroke();
+
+  // zīmē temperatūras/peļņas krāsaino līkni
+  ctx.beginPath();
+  ctx.lineWidth = 3;
+
+  let firstPoint = true;
+  for (const [temp, rate] of data) {
+    const x = xScale(temp);
+    const y = yScale(rate);
+
+    const color = tempSegments.find(([t]) => temp <= t)?.[1]?.color;
+
+    if (firstPoint) {
+      ctx.moveTo(x, y);
+      ctx.strokeStyle = color;
+      firstPoint = false;
+    } else {
+      ctx.lineTo(x, y);
+    }
+
+    // krāsa mainās, tātad jāzīmē
+    if (ctx.strokeStyle !== color) {
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.strokeStyle = color;
+    }
+  }
+
+  ctx.stroke();
+
+  // zīmē pašreizējās temperatūras punktiņu
+  ctx.beginPath();
+  ctx.fillStyle = colors.dot;
+  ctx.arc(currentX, currentY, 5, 0, Math.PI * 2);
+  ctx.fill();
+
+  return await canvas.encode('png');
+}
+
+// funckija kopējās peļņas aprēķinam konkrētā laikā
+// veikt aprēķinu ik 6 minūtes (maxStepSizeMs)
+// ja ir mazāk par 6 minūtēm (vai pēdējais solis), tad tiks izmantots atlikums
 function calculateProfit(elapsedMs: number, lastTemp: number) {
-  // veikt aprēķinu ik 6 minūtes
-  // ja ir mazāk par 6 minūtēm (vai pēdējais solis), tad tiks izmantots atlikums
   const maxStepSizeMs = Config.MS_IN_ONE_HOUR / 10;
 
   let sum = 0;
@@ -75,7 +388,6 @@ function calculateProfit(elapsedMs: number, lastTemp: number) {
       (Math.abs(Config.MIN_TEMPERATURE - newTemp) / Config.TEMPERATURE_DECAY_PER_HOUR) * Config.MS_IN_ONE_HOUR;
 
     const step = Math.min(maxStepSizeMs, diffence, msUntilLowestTemp);
-    // console.log({ step });
 
     sum += (step / Config.MS_IN_ONE_HOUR) * calculateEarningRatePerHour(newTemp);
     diffence -= step;
@@ -96,16 +408,6 @@ function calculateEarningRatePerHour(temp: number): number {
 
 function calculateTemperatureDecay(elapsedMs: number): number {
   return (elapsedMs / Config.MS_IN_ONE_HOUR) * Config.TEMPERATURE_DECAY_PER_HOUR;
-}
-
-function getData(user: UserProfile) {
-  const { currentLati, lastTemp, lastUpdateTime } = user.properties.metalluznuNodosanasPunkts;
-
-  return {
-    currentLati: Math.floor(currentLati),
-    currentTemp: lastTemp,
-    maxLati: Config.BASE_MAX_LATI,
-  };
 }
 
 const enum UpdateAction {
@@ -162,7 +464,9 @@ async function updateDb(
     return { ok: false };
   }
 
-  // state.metalluznuNodosanasPunkts.chart = await generateEarningRateChart(newTemp);
+  const now = performance.now();
+  state.metalluznuNodosanasPunkts.chart = await generateChart(newTemp);
+  console.log('Granulu katla bildes ģenerācija:', performance.now() - now);
 
   return { ok: true, data: { user: values.at(-1)!, latiToAdd } };
 }
@@ -172,13 +476,16 @@ async function init(state: IpasumiState): Promise<{ ok: boolean }> {
   //   return { ok: true };
   // }
 
-  // await generateChartData();
+  await generateChartData();
 
   const res = await updateDb(state);
 
   if (res.ok) {
     state.user = res.data.user;
   }
+
+  // const chart = await generateEarningRateChart(state.user.properties.metalluznuNodosanasPunkts.lastTemp);
+  // state.metalluznuNodosanasPunkts.chart = chart;
 
   return res;
 }
@@ -199,14 +506,30 @@ const enum ComponentId {
   Refresh = 'ipasumi_metalluznuNodosanasPunkts_refresh',
 }
 
+// funkcija 
+function getData(user: UserProfile) {
+  const { currentLati, lastTemp } = user.properties.metalluznuNodosanasPunkts;
+
+  return {
+    currentLati: Math.floor(currentLati),
+    currentTemp: lastTemp,
+    maxLati: Config.BASE_MAX_LATI,
+  };
+}
+
 function view(state: IpasumiState, i: BaseInteraction) {
   const data = getData(state.user);
 
   const row = new ActionRowBuilder<ButtonBuilder>();
 
-  row.addComponents(
-    new ButtonBuilder().setLabel(`Izņemt latus`).setStyle(ButtonStyle.Success).setCustomId(ComponentId.IznemtLatus),
-  );
+  if (data.currentLati > 0) {
+    row.addComponents(
+      new ButtonBuilder()
+        .setLabel(`Izņemt latus (${data.currentLati})`)
+        .setStyle(ButtonStyle.Success)
+        .setCustomId(ComponentId.IznemtLatus),
+    );
+  }
 
   row.addComponents(
     new ButtonBuilder()
@@ -216,22 +539,34 @@ function view(state: IpasumiState, i: BaseInteraction) {
       .setEmoji(emoji('granulas')),
   );
 
+  // poga izstrādei lai atjauninātu datus
   row.addComponents(
     new ButtonBuilder().setStyle(ButtonStyle.Secondary).setCustomId(ComponentId.Refresh).setEmoji('🔄'),
   );
 
+  const tempText = tempSegments.find(([t]) => data.currentTemp <= t)?.[1]?.text;
+
   return embedTemplate({
     i,
+    content: '\u200B',
     title: 'Metāllūžņu nodošanas punkts',
-    description:
-      `Temperatūra: **${data.currentTemp.toFixed(1)}** °C\n` +
-      `Lati: ${data.currentLati}/${data.maxLati}\n` +
-      `Pašreizējā peļņa: ${calculateEarningRatePerHour(data.currentTemp)} lati/h\n\n` +
-      '### test\ntest\ntest\n### test\ntest',
+    description: '',
+    color: resolveColor('#fdba74'),
     fields: [
-      { name: 'granulu katls', value: 'test', inline: false },
-      { name: '-# granulu katls', value: 'test', inline: false },
+      {
+        name: 'Granulu katls:',
+        value: `Temperatūra: **${data.currentTemp.toFixed(1)}** °C\n${tempText}`,
+        inline: true,
+      },
+      {
+        name: '\u200B',
+        value:
+          `Lati: **${data.currentLati}**/${data.maxLati}\n` +
+          `Pašreizējā peļņa: **${calculateEarningRatePerHour(data.currentTemp).toFixed(2)}** lati/h\n`,
+        inline: true,
+      },
     ],
+
     components: [row],
     files: state.metalluznuNodosanasPunkts.chart
       ? [new AttachmentBuilder(state.metalluznuNodosanasPunkts.chart, { name: 'granulu_katls_grafiks.png' })]
@@ -263,13 +598,17 @@ async function handler(
     return { update: true };
   }
 
-  const itemInInv = state.user.items.find(({ name }) => name === 'granulas');
+  // TODO: pārbaudīt vai lietotājam ir granulas un tās noņemt no inventāra izmantojot
+  // const itemInInv = state.user.items.find(({ name }) => name === 'granulas');
 
   if (i.customId === ComponentId.IznemtLatus) {
     const res = await updateDb(state, UpdateAction.IznemtLatus);
     if (!res.ok) return { error: true };
 
     state.user = res.data.user;
+
+    // const chart = await generateEarningRateChart(state.user.properties.metalluznuNodosanasPunkts.lastTemp);
+    // state.metalluznuNodosanasPunkts.chart = chart;
 
     intReply(
       i,
